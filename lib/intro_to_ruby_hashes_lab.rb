3065e373046256fcf111a237c1564ef8d16bcfde
Cody C <codyicastro@gmail.com>
2020-06-29 17:51:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,12 @@ def new_hash
 end
 
 def my_hash
-  {:food = Nachos
+  {:food = Nachos}
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  {:name = Grace Hopper}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
