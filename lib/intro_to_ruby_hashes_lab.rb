bcc1bd81fe2c2bf7dc2d7c5d5bfe921ffe0c01c2
Cody C <codyicastro@gmail.com>
2020-06-29 17:51:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,17 +3,17 @@ def new_hash
 end
 
 def my_hash
-  {:food = Nachos}
+  {:food => Nachos}
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
-  {:name = Grace Hopper}
+  {:name => Grace Hopper}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
-  {:num = (number)}
+  {:num => (number)}
   # return a hash with a key :id assigned to the provided number
 end
